54c92f1c6ee8541963e996f3051a933367f70693
Lazaro Junior <ljrlazaro@hotmail.com>
2024-04-18 23:11:08 -0300

    feat: Criação de novas rotas, metodos rest para busca de usuários por id e criação do usuário no banco

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,8 @@
 import admin from "../db/connection.js"
 
 const userController = {
-    create: async (request, response) => {
-        console.log("Create User");
+    getAllUsers: async (request,response) => {
+        console.log("Get All User");
 
         admin
             .firestore()
@@ -14,8 +14,43 @@ const userController = {
                     uid: doc.id,
                 }));
                 response.json(users);
+            
             });
+    },
+
+    getUserById: async (request, response) => {
+        console.log("Get User by Id");
+        console.log(request.params);
+        const userSelected = await admin.firestore().collection("Users").doc(request.params.id).get();
+        console.log(userSelected);
+        response.json(userSelected.data());
+    },
+
+    
+
+    createUser: async (request, response) => {
+        console.log("Create User");
+        const user = {
+            email: request.body.email,
+            name: request.body.name,
+            password: request.body.password,
+          };
+
+        try {
+            const docRef = await admin
+                .firestore()
+                .collection("Users")
+                .add(user);
+            
+            response.json({status: "Usuário criado com su sexo!", userId: docRef.id})
+        } catch (error) {
+            response.json({status: "Deu errado viu!?", Message: error})
+        }  
+        
+
+            response.json(response.snapshot);
     }
 }
+
 
 export default userController
--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -4,6 +4,10 @@ const router = express.Router()
 
 import userController from "../controllers/userController.js"
 
-router.route("/users").post((req, res) =>userController.create(req, res))
+router.route("/users").get((req, res) =>userController.getAllUsers(req, res))
+
+router.route("/users").post((req, res) =>userController.createUser(req, res))
+
+router.route("/users/:id").get((req, res) =>userController.getUserById(req, res))
 
 export default router
