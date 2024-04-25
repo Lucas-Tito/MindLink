7d6980e7e2b56a8d6a3454172b27352bcbd5b9da
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2024-04-25 19:48:46 -0300

    feat: finishing the user registration screen

--- Frontend/frontend/src/components/Register/index.jsx
+++ Frontend/frontend/src/components/Register/index.jsx
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import "./style.css";
 import cerebro_icon from "../../assets/cerebro.png";
 import imagem_consulta from "../../assets/imagemConsulta.png";
-import personIcon from "../../assets/personIcon.png";
+import personIcon from "../../assets/personIcon.svg";
+import ajudaIcon from "../../assets/ajuda.png";
+import senhaIcon from "../../assets/senha.svg";
 
 export default function Register() {
   // Definindo os estados para os campos do formulário
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
 
   // Função para lidar com o envio do formulário
   const handleSubmit = (event) => {
@@ -49,6 +56,8 @@ export default function Register() {
               />
             </div>
             <div>
+              <img src={senhaIcon} class="input-icon1" />
+
               <input
                 type="password"
                 placeholder="Senha"
@@ -57,8 +66,20 @@ export default function Register() {
                 onChange={(e) => setSenha(e.target.value)}
               />
             </div>
+            <div class="checkbox-container">
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+              />
+              <label
+                className={isChecked ? "checkbox-label2" : "checkbox-label1"}
+              >
+                Sou um psicólogo
+              </label>
+            </div>
             <button type="submit" className="btn1">
-              Cadastrar
+              CADASTRAR
             </button>
           </form>
         </div>
@@ -66,6 +87,29 @@ export default function Register() {
       <div class="image-container">
         <img src={imagem_consulta} alt="Imagem" />
       </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "85%",
+          left: "47.1%",
+          width: "60px",
+          height: "60px",
+          background: "#8F1EA8",
+          backgroundImage: `url(${ajudaIcon})`,
+          borderRadius:
+            "50% 0 0 50%" /* Apenas a parte esquerda é um semicírculo */,
+        }}
+      ></div>
+      <img
+        style={{
+          position: "absolute",
+          top: "85.3%",
+          left: "47.7%",
+          width: "50px",
+          height: "50px",
+        }}
+        src={ajudaIcon}
+      />
     </div>
   );
 }
