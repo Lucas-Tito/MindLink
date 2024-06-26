db771c8216ce582b1b7dc8e9d3b9bd76460ca47d
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2024-06-26 19:48:50 -0300

    fix: appjs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,13 +6,25 @@ import CardChat from "./components/CardChat";
 import { EditProfile } from "./components/Edit";
 import CardPsycho from "./components/CardPsycho/CardPsycho";
 import Scheduling from "./components/Scheduling";
-import { Login } from "./components/Login/login";
+import Login from "./components/Login/login";
 import Calendar from "./components/Calendar/Calendar";
+import SignOut from "./components/Login/SignOut";
+import SignIn from "./components/Login/SignIn";
+import firebase from "firebase/compat/app";
+import { auth } from "./firebase"; // Importando auth do arquivo de configuraçãoimport ChatRoom from "./components/Chat/ChatRoom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import ChatRoom from "./components/Chat/ChatRoom";
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <Calendar />
+      <header>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
