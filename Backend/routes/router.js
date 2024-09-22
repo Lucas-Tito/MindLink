e0e7c98ffa0a120ce0c6b2617b2e5393cb7a17a6
Lazaro Junior <ljrlazaro@hotmail.com>
2024-09-22 19:35:45 -0300

    rota de pesquisa

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -10,11 +10,13 @@ import userConnectionRouter from "./usersConnetionRouter.js"
 import appointmentRouter from "./appointmentRouter.js"
 import messagesRouter from "./messagesRouter.js"
 import avaliabilityRouter from "./availabilityRouter.js"
+import searchPsychologistsRouter from "./searchPsychologistsRouter.js"
 
 router.use("/", userRouter)
 router.use("/", userConnectionRouter)
 router.use("/", appointmentRouter)
 router.use("/", messagesRouter)
 router.use("/", avaliabilityRouter)
+router.use("/", searchPsychologistsRouter)
 
 export default router
